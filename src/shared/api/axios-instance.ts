dd16703ed52a40ad070d2f8b261a52f0f3fe2fd4
Roman <skorohodroman921@gmail.com>
2024-11-11 06:27:11 +0300

    add Express url server

--- src/shared/api/axios-instance.ts
+++ src/shared/api/axios-instance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "http://i0oos0ks8cgc4004sok8kwwk.91.210.170.174.sslip.io",
 });
 
 // instance.interceptors.request.use((config) => {
